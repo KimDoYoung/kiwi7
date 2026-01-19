3ceb2e3de310a697e89b4a825a2add7f821fedd3
KimDoYoung <kdy987@gmail.com>
2026-01-19 22:41:34 +0900

    launch.json 수정 및 KIS 요청/응답 모델 제거, 외국인 매매종목과 테스트 페이지 추가

--- backend/domains/kis/models/requests/__init__.py
+++
@@ -1,4 +0,0 @@
-from .market import MARKET_REQUESTS
-from .market_etf import MARKET_ETF_REQUESTS
-from .market_realtime import MARKET_REALTIME_REQUESTS
-from .trading import TRADING_REQUESTS
--- backend/domains/kis/models/responses/__init__.py
+++
@@ -1,3 +0,0 @@
-from .market import MARKET_RESPONSES
-from .misc import MISC_RESPONSES
-from .trading import TRADING_RESPONSES
